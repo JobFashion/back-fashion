2e33773099aca799b5e69b94a7f736858e2d075b
Markos <markosmk@me.com>
2022-03-25 20:29:36 -0300

    update: minor changes for build with cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import { handleErrors } from './middleware/handleError.js'
 import allRoutes from './routes'
 const app = express()
@@ -8,6 +9,7 @@ app.set('port', process.env.PORT || 8000)
 
 // middlewares
 app.use(express.json())
+app.use(cors())
 
 // routes
 app.use('/api/v1', allRoutes)
