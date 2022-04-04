cc8c45b3c43e56eae8fe49573ec484ea1a4467ce
Markos <markosmk@me.com>
2022-04-04 16:00:26 -0300

    update: add status for unification error messages

--- src/middleware/handleError.js
+++ src/middleware/handleError.js
@@ -1,15 +1,17 @@
+// import { AppError } from '../utils/AppError'
+
 export const handleErrors = (err, req, res, next) => {
   if (!err.statusCode) err.statusCode = 500
+  // if (err instanceof AppError) {
+  // if (err.name === 'UnauthorizedError') {
+  //   return res.status(401).json({ message: 'invalid token...' })
+  // }
+  // if (err.name === 'CastError') {
+  //   return res.status(400).json({ message: 'invalid id...' })
+  // }
 
-  if (err.name === 'UnauthorizedError') {
-    return res.status(401).json({ message: 'invalid token...' })
-  }
-  if (err.name === 'CastError') {
-    return res.status(400).json({ message: 'invalid id...' })
-  }
-
-  // default to 500 server error
   return res.status(err.statusCode).json({
+    name: err.name,
     status: err.statusCode,
     message: err.message
   })
