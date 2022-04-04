7c4aadfc82f39295220025449fb25fec5a229a4e
Markos <markosmk@me.com>
2022-04-04 15:59:27 -0300

    add: validation of request

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,11 +1,21 @@
 import { Router } from 'express'
 
 import * as authController from '../controllers/auth'
+import { validateReq } from '../middleware/validateReq'
 
 const router = Router()
 
 // -> auth
-router.post('/login', authController.login)
-router.post('/register', authController.register)
+router.post(
+  '/login',
+  validateReq(authController.validate('loginUser')),
+  authController.login
+)
+
+router.post(
+  '/register',
+  validateReq(authController.validate('registerUser')),
+  authController.register
+)
 
 export default router
